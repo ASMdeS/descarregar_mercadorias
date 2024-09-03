28f169b61ca075d9603bd3eab69e50db91dd6d8a
Arthur Santos Marinho de Souza <arthur.souza2005@hotmail.com>
2024-09-03 14:22:16 -0300

    Informações Completas

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,57 +30,77 @@ if os.path.exists(csv_file_path):
 else:
     # Create an empty dataframe with the necessary columns
     df = pd.DataFrame(
-        columns=["ID", "Fornecedor", "Número da NF", "Drop-off Date", "Drop-off Time", "Status", "Centro de Distribuição", "Tipo de Carga",
+        columns=["ID", "Fornecedor", "Número da NF", "Drop-off Date", "Drop-off Time", "Status",
+                 "Centro de Distribuição", "Tipo de Carga",
                  "Número de Pallets", "Peso Total", "Número de SKUs"])
     df.to_csv(csv_file_path, index=False)  # Save the initial dataframe
 
 # Section to add a new schedule
-st.header("Add a New Schedule")
+st.header("Adicionar Agendamento")
 
 with st.form("add_schedule_form"):
     supplier_name = st.text_input("Fornecedor")
     product = st.text_input("Número da NF")
     dropoff_date = st.date_input("Drop-off Date", min_value=datetime.date.today())
     dropoff_time = st.time_input("Drop-off Time")
-    status = st.selectbox("Status", ["Scheduled", "Completed", "Cancelled"])
+    status = st.selectbox("Status", ["Agendado", "Completo", "Cancelado"])
     distribution_center = st.selectbox("Centro de Distribuição", ["CLAS", "GPA", "JSL"])
     load_type = st.selectbox("Tipo de Carga", ["Pallet Monoproduto", "Pallet Misto", "Estivado"])
     pallet_number = st.number_input("Número de Pallets", step=1)
     total_weight = st.number_input("Peso Total", step=1)
     sku_number = st.number_input("Número de SKUs", step=1)
-    submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Enviar")
+
+# Define the maximum number of schedules allowed per day
+max_schedules_per_day = 5
 
 if submitted:
-    recent_schedule_id = int(df["ID"].max()[8:]) + 1 if not df.empty else 1
-    new_schedule = {
-        "ID": f"SCHEDULE-{recent_schedule_id}",
-        "Fornecedor": supplier_name,
-        "Número da NF": product,
-        "Drop-off Date": dropoff_date,
-        "Drop-off Time": dropoff_time.strftime("%H:%M"),
-        "Status": status,
-        "Centro de Distribuição": distribution_center,
-        "Tipo de Carga": load_type,
-        "Número de Pallets": pallet_number,
-        "Peso Total": total_weight,
-        "Número de SKUs": sku_number
-    }
+    # Check how many schedules are already set for the selected drop-off date
+    schedules_on_date = df[(df["Drop-off Date"] == str(dropoff_date)) &
+                           (df["Centro de Distribuição"] == distribution_center)]
+    if len(schedules_on_date) >= max_schedules_per_day:
+        st.error(
+            f"Não é possível agendar mais de {max_schedules_per_day} entregas para o dia {dropoff_date} por Centro de Distribuição.")
+    else:
+        recent_schedule_id = int(df["ID"].max()[8:]) + 1 if not df.empty else 1
+        new_schedule = {
+            "ID": f"SCHEDULE-{recent_schedule_id}",
+            "Fornecedor": supplier_name,
+            "Número da NF": product,
+            "Drop-off Date": dropoff_date,
+            "Drop-off Time": dropoff_time.strftime("%H:%M"),
+            "Status": status,
+            "Centro de Distribuição": distribution_center,
+            "Tipo de Carga": load_type,
+            "Número de Pallets": pallet_number,
+            "Peso Total": total_weight,
+            "Número de SKUs": sku_number
+        }
 
-    df_new = pd.DataFrame([new_schedule])
+        df_new = pd.DataFrame([new_schedule])
 
-    st.write("Schedule submitted! Here are the schedule details:")
-    st.dataframe(df_new, use_container_width=True, hide_index=True)
+        st.write("Agendamento enviado! Aqui estão os detalhes:")
+        st.dataframe(df_new, use_container_width=True, hide_index=True)
 
-    df = pd.concat([df_new, df], axis=0)
-    df.to_csv(csv_file_path, index=False)  # Save the updated dataframe
+        df = pd.concat([df_new, df], axis=0)
+        df.to_csv(csv_file_path, index=False)  # Save the updated dataframe
+
 
 # Section to view and edit existing schedules
-st.header("Existing Schedules")
-st.write(f"Number of schedules: `{len(df)}`")
+st.header("Agendamentos Existentes")
+st.write(f"Número de Agendamentos: `{len(df)}`")
+
+# Add a selectbox to filter by Distribution Center
+distribution_center_filter = st.selectbox("Filtrar por Centro de Distribuição",
+                                          ["Todos"] + df["Centro de Distribuição"].unique().tolist())
+
+# Filter the dataframe based on the selected Distribution Center
+if distribution_center_filter != "Todos":
+    df = df[df["Centro de Distribuição"] == distribution_center_filter]
 
 st.info(
-    "You can edit the schedules by double-clicking on a cell. Note how the plots below "
-    "update automatically! You can also sort the table by clicking on the column headers.",
+    "Você pode editar os agendamentos ao clicar duas vezes em uma célula. Perceba como os gráficos "
+    "são atualizados automaticamente! Você também pode ordenar a tabela ao clicar no cabeçalho das colunas.",
     icon="✍️",
 )
 
@@ -92,11 +112,12 @@ edited_df = st.data_editor(
         "Status": st.column_config.SelectboxColumn(
             "Status",
             help="Schedule status",
-            options=["Scheduled", "Completed", "Cancelled"],
+            options=["Agendado", "Completo", "Cancelado"],
             required=True,
         ),
     },
-    disabled=["ID", "Fornecedor", "Número da NF", "Drop-off Date", "Drop-off Time"],
+    disabled=["ID", "Fornecedor", "Número da NF", "Drop-off Date", "Drop-off Time", "Centro de Distribuição",
+              "Tipo de Carga", "Número de Pallets", "Peso Total", "Número de SKUs"],
 )
 
 # Save edits back to CSV
@@ -108,10 +129,10 @@ if edited_df is not None:
 st.header("Statistics")
 
 col1, col2, col3 = st.columns(3)
-num_scheduled = len(df[df.Status == "Scheduled"])
+num_scheduled = len(df[df.Status == "Agendado"])
 col1.metric(label="Number of scheduled drop-offs", value=num_scheduled, delta=5)
-col2.metric(label="Completed drop-offs", value=len(df[df.Status == "Completed"]))
-col3.metric(label="Cancelled drop-offs", value=len(df[df.Status == "Cancelled"]))
+col2.metric(label="Completo drop-offs", value=len(df[df.Status == "Completo"]))
+col3.metric(label="Cancelado drop-offs", value=len(df[df.Status == "Cancelado"]))
 
 st.write("")
 st.write("##### Drop-off status per month")
